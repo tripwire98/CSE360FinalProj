ab2b05ae538cb7cd356463bffa750b43f328478b
BrandonO <brandonobier1998@gmail.com>
2019-11-25 20:19:16 -0700

    file reader

--- src/FinalProject/WordProcessor.java
+++ src/FinalProject/WordProcessor.java
@@ -1,22 +1,22 @@
 package FinalProject;
+import java.io.File; 
+import java.io.*;
+import java.util.Scanner; 
 import java.util.*;
 
 public class WordProcessor {
-    //////////////////////////////////////////////////////////////////
-	//Just for testing purposes to make sure github was updateing
-	
-	public void dispMessage()
-    {
-        System.out.println("Hello World.");
-    }
-     
-    //Main method
-    public static void main(String s[])
-    {
-        //create object of HelloWorld Class
-    	WordProcessor obj = new WordProcessor();
-        obj.dispMessage();
-    }
-    
-    ///////////////////////////////////////////////////////////////////
+	public static void main(String[] args)throws Exception 
+	 { 
+		//change the file location to where the test.txt is on your desktop, we will implement a system that works better
+		//for getting the location later on but this works for now
+		String fileLocation = "C:\\Users\\brand\\Desktop\\test.txt";
+	    File file = new File(fileLocation); 
+	  
+	    BufferedReader br = new BufferedReader(new FileReader(file)); 
+	  
+	    String st; 
+	    while ((st = br.readLine()) != null) {
+	      System.out.println(st); 
+	    }
+	 }
 }
