1ed5a2f3aaec77f5b3d98b25b4c1d53a4b472881
mstampar <56246799+mstampar@users.noreply.github.com>
2019-11-26 11:47:51 -0700

    working1

--- FinalProject/src/FinalProject/WordProcessor.java
+++ FinalProject/src/FinalProject/WordProcessor.java
@@ -18,6 +18,23 @@ public class WordProcessor {
 	    while ((st = br.readLine()) != null) {
 	      System.out.println(st); 
 	    }
+	   //we read the characters in the string using scanner?
+	    Scanner in = new Scanner(st);
+	    //creating an array holding the commands of a size 100 and if there is more commands we can double it?
+	    String[] commands = new String[100];
+	    //an integer holding the count of the characters
+	    int numChar = 0;
+	   //read the string character by character until the size of the string
+	    while(numChar < st.length())
+	    {
+	    	String currentChar = Character.toString(in.next().charAt(0));
+	    	String currentCommand;
+	    	//if the read character is dash then read until emptyspace? and add into command array?
+	    	if(currentChar.contentEquals("-"))
+	    	{
+	    		//
+	    	}
+	    }
 	    
 	 }
 }
