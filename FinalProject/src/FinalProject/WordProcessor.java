c8b5ed80e562983ff7a1fa7d9f4c6c8e002eda09
mstampar <56246799+mstampar@users.noreply.github.com>
2019-11-26 22:48:46 -0700

    Update WordProcessor.java

--- FinalProject/src/FinalProject/WordProcessor.java
+++ FinalProject/src/FinalProject/WordProcessor.java
@@ -114,7 +114,7 @@ public class WordProcessor {
 	    				//if it is left justified and number of characters is 0 we print the number of indents and then the current character
 	    				for(int j = 0; j<paragraph; j++)
 	    					currentOutputLine = currentOutputLine + " ";
-	    				currentOutputline = currentOutputLine + currentChar;
+	    				currentOutputLine = currentOutputLine + currentChar;
 	    			}
 	    		}
 	    		
@@ -123,76 +123,9 @@ public class WordProcessor {
 		    int numChar = 0;
 		    int numWords = 0;
 		   
-		   //read the string character by character until the size of the string
-	    while(numChar < st.length())
-	    {
-	    	
-	    	String currentCommand="";
-	    	//if the read character is dash then read until empty space? and add into command array? and we dont need to increase the numChar count because we are not putting them into output txt?
-	    	//also we need to be checking if we are not in the actual txt but we are on the command line because the input is read a s one string
-	    	if(currentChar.equals("-"))
-	    	{
-	    		currentCommand=currentCommand+currentChar;
-	    		currentChar = Character.toString(in.next().charAt(0));
-	    		while (!currentChar.equals(" ")) {
-	    			currentCommand=currentCommand+currentChar;
-	    			currentChar = Character.toString(in.next().charAt(0));
-	    		}
-	    		//i think we need to be checking if the command array is full and if so doubling it
-	    		if (numCommands<100)
-	    		{
-	    			commands[numCommands]=currentCommand;
-	    			numCommands++;	
-	    		}
-	    		else 
-	    		{
-	    			//double the array and copy it code missing for now
-	    			String[] tempArray = new String[commands.length*2];
-	    			for(int i=0; i<commands.length; i++)
-	    				tempArray[i]=commands[i];
-	    			commands = tempArray;
-	    			commands[numCommands]=currentCommand;
-		    		numCommands++;	
-	    		}
-	    	}
-	    	//if it is not a command either start storing the characters as words somewhere or lines ?!?
-	    	//or we can do this whole part once all commands are read and we know how many characters per line/if wrapping is on
-	    	else 
-	    	{
-	    		String word = currentChar;
-	    		//making an array to hold a line of words?
-	    		String[] line = new String[100];
-	    		//we need to count the character
-	    		numChar++;
-	    		word=word+currentChar;
-	    		currentChar = Character.toString(in.next().charAt(0));
-	    		while (!currentChar.equals(" ")) {
-	    			word=word+currentChar;
-	    			currentChar = Character.toString(in.next().charAt(0));	
-	    			numChar++;
-	    		}
-	    		//checking if the array for the line is full and f number of characters is exactly divisable by 100s
-	    		if ((numWords+1)%100!=0 && numChar%100!=0)
-	    		{
-	    			
-	    			line[(numWords+1)%100]=word;
-	    			numWords++;	
-	    		}
-	    		else if(numChar%100!=0) 
-	    		{
-	    			//double the array and copy it code missing for now
-	    			String[] tempArray = new String[line.length*2];
-	    			for(int i=0; i<line.length; i++)
-	    				tempArray[i]=line[i];
-	    			line = tempArray;
-	    			line[(numWords+1)%100]=word;
-	    			numWords++;	
-	    		}
-	    		else
-	    			//need to start a new line
-	    	}
+	    
 	    }
-	    }
+
 	    
 	 }
 }
